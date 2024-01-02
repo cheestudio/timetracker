15e6e23220616a3132a096588694bc074b98fb79
Lars Faye <lars@goodchee.com>
2024-01-01 22:58:56 -0500

    TS errors

--- src/app/api/clients/route.ts
+++ src/app/api/clients/route.ts
@@ -1,18 +1,22 @@
 import { supabase } from "@/lib/utils";
 
-export async function POST (request: Request) {
-  
-  try {
-    const { data, error } = await supabase
-    .from('Clients')
-    .select('*')
-    .order('client_name', { ascending: true });
-  if (error) {
-    console.error('Error fetching data: ', error);
-    return [];
-  }
-    return Response.json(data);
-  } catch (error) {
-    Response.json({ error: (error as Error).message },{status: 500});
-  }
+export async function POST(request: Request): Promise<Response> {
+    try {
+        const { data, error } = await supabase
+            .from('Clients')
+            .select('*')
+            .order('client_name', { ascending: true });
+        
+        if (error) throw error;
+
+        return new Response(JSON.stringify(data), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    } catch (error) {
+        return new Response(JSON.stringify({ error: (error as Error).message }), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
 }
