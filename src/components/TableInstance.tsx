492a92a4a1e3ab1fef447338265315e2cd676044
Lars Faye <lars@goodchee.com>
2024-01-05 21:54:23 -0500

    refactoring timer

--- src/components/TableInstance.tsx
+++ src/components/TableInstance.tsx
@@ -1,96 +1,37 @@
 "use client";
 
-import { unstable_noStore as noStore } from 'next/cache';
-
-import React, { useState, useEffect, useCallback, useMemo } from "react";
-
-import { Table, TableHeader, TableBody, TableColumn, TableRow, TableCell, Button, SortDescriptor, Pagination, Select, SelectItem } from "@nextui-org/react";
-
+import { useState, useEffect, useMemo } from "react";
+import { TimeEntryProps, SortDirection } from "@/lib/types";
+import { Button, SortDescriptor } from "@nextui-org/react";
+import { convertTime, getTodayRange, getThisWeekRange, getThisMonthRange } from "@/lib/utils";
 import SubmitTime from "./SubmitTime";
 import { supabase } from "@/lib/utils";
 import toast from 'react-hot-toast';
-
-interface TimeEntryProps {
-  key: string;
-  date: string;
-  task: string;
-  time_tracked: string;
-  entry_id: string;
-  owner: string;
-}
-
-type SortDirection = 'ascending' | 'descending' | undefined;
+import PaginateTable from './PaginateTable';
+import TableDisplay from './TableDisplay';
+import TableRowControls from './TableRowControls';
 
 const TableInstance = ({ client }: { client: string }) => {
 
-  noStore();
-
   /* Time Converison
   ========================================================= */
-
   const [calculatedTime, setCalculatedTime] = useState(0);
-
-  const convertTime = (time: string) => {
-    const hours = Math.floor(parseInt(time, 10) / 60);
-    const minutes = parseInt(time, 10) % 60;
-    return `${hours}h ${minutes}m`;
-  }
 
   const calculateTotalHours = (entries: TimeEntryProps[]) => {
     let totalHours = 0;
+    if(entries.length === 0) {
+      return;
+    }
     entries.forEach((entry) => {
-      totalHours += parseInt(entry.time_tracked, 10);
+      totalHours += parseInt(entry?.time_tracked, 10);
     });
     setCalculatedTime(totalHours);
   };
 
-  function formatDate(dateString: string) {
-    const [year, month, day] = dateString.split('-').map(part => parseInt(part, 10));
-    const date = new Date(year, month - 1, day);
-    const formattedMonth = String(date.getMonth() + 1);
-    const formattedDay = String(date.getDate());
-    const formattedYear = date.getFullYear();
-    return `${formattedMonth}/${formattedDay}/${formattedYear}`;
-  }
-  
-
   /* Time Entries
   ========================================================= */
   const [timeEntries, setTimeEntries] = useState([] as any);
-  const [tableKey, setTableKey] = useState(0);
   const [selectedDateRange, setSelectedDateRange] = useState("all");
-
-  const getTodayRange = () => {
-    const today = new Date();
-    today.setHours(0, 0, 0, 0);
-    const end = new Date(today);
-    end.setHours(23, 59, 59, 999);
-    console.log('today', today, end);
-    return [today, end];
-  };
-
-  const getThisWeekRange = () => {
-    const today = new Date();
-    const dayOfWeek = today.getDay();
-
-    const start = new Date(today);
-    start.setDate(today.getDate() - dayOfWeek);
-    start.setHours(0, 0, 0, 0);
-    
-    const end = new Date(start);
-    end.setDate(start.getDate() + 6);
-    end.setHours(23, 59, 59, 999);
-    return [start, end];
-  };
-
-
-  const getThisMonthRange = () => {
-    const date = new Date();
-    const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0, 23, 59, 59, 999);
-    console.log('month', firstDay, lastDay);
-    return [firstDay, lastDay];
-  };
 
   const handleDateRange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedDateRange(e.target.value);
@@ -119,10 +60,7 @@ const TableInstance = ({ client }: { client: string }) => {
             .lte('date', range[1].toISOString());
         }
       }
-
-      noStore();
       const { data, error } = await query;
-
       if (error) {
         console.error('Error fetching data: ', error);
         return [];
@@ -132,13 +70,11 @@ const TableInstance = ({ client }: { client: string }) => {
         calculateTotalHours(data);
       }
     };
-
     fetchTimeEntries();
-
     const handleNewEntry = () => {
       fetchTimeEntries();
     };
-
+    
     window.addEventListener('timeEntryAdded', handleNewEntry);
 
     return () => {
@@ -149,7 +85,7 @@ const TableInstance = ({ client }: { client: string }) => {
 
   /* Selected Rows
   ========================================================= */
-  const [selectedKeys, setSelectedKeys] = React.useState([] as any);
+  const [selectedKeys, setSelectedKeys] = useState([] as any);
   const handleSelectedKeys = (keys: any) => {
     if (keys === 'all') {
       if (selectedKeys.length === timeEntries.length) {
@@ -184,7 +120,6 @@ const TableInstance = ({ client }: { client: string }) => {
     }
     window.dispatchEvent(new CustomEvent('timeEntryAdded'));
     setSelectedKeys([]);
-    setTableKey((prevKey) => prevKey + 1);
     toast.success('Removed');
   }
 
@@ -194,11 +129,8 @@ const TableInstance = ({ client }: { client: string }) => {
     column: "",
     direction: undefined as SortDirection,
   });
-
   const sort = (column: any) => {
-
     let newDirection: SortDirection = 'ascending';
-
     if (column.column === sortDescriptor.column && sortDescriptor.direction === 'ascending') {
       newDirection = 'descending';
     }
@@ -207,12 +139,9 @@ const TableInstance = ({ client }: { client: string }) => {
       direction: newDirection,
     };
     setSortDescriptor(colSort);
-
     const sortedEntries = [...timeEntries].sort((a, b) => {
-
       let valueA = a[colSort.column];
       let valueB = b[colSort.column];
-
       if (colSort.column === 'date') {
         valueA = new Date(valueA);
         valueB = new Date(valueB);
@@ -226,7 +155,6 @@ const TableInstance = ({ client }: { client: string }) => {
       } else {
         return valueA > valueB ? -1 : 1;
       }
-
     });
     setTimeEntries(sortedEntries);
   }
@@ -251,104 +179,51 @@ const TableInstance = ({ client }: { client: string }) => {
     setPaginationKey((prevKey) => prevKey + 1);
   }
 
-
   return (
     <div className="flex flex-col gap-8 table-instance">
-      <div className="flex justify-end gap-5">
-        <div className="flex-[0_0_200px]">
-          <Select
-            value={viewableRows}
-            onChange={handleViewableRows}
-            variant="bordered"
-            label="Viewable Rows"
-            labelPlacement="outside"
-            placeholder="Select"
-            popoverProps={{
-              classNames: {
-                content: "bg-[#27272A]",
-              },
-            }}
-          >
-            <SelectItem key="-1">All</SelectItem>
-            <SelectItem key="25">25</SelectItem>
-            <SelectItem key="50">50</SelectItem>
-            <SelectItem key="75">75</SelectItem>
-            <SelectItem key="100">100</SelectItem>
-          </Select>
-        </div>
-        <div className="flex-[0_0_200px]">
-          <Select
-            value={selectedDateRange}
-            onChange={handleDateRange}
-            variant="bordered"
-            label="Date Range"
-            labelPlacement="outside"
-            placeholder="Select"
-            popoverProps={{
-              classNames: {
-                content: "bg-[#27272A]",
-              },
-            }}
-          >
-            <SelectItem key="all">All</SelectItem>
-            <SelectItem key="today">Today</SelectItem>
-            <SelectItem key="this_week">This Week</SelectItem>
-            <SelectItem key="this_month">This Month</SelectItem>
-          </Select>
-        </div>
-      </div>
-      <Table
-        selectionMode="multiple"
-        onSelectionChange={handleSelectedKeys}
+
+      <TableRowControls
+        viewableRows={viewableRows}
+        selectedDateRange={selectedDateRange}
+        handleViewableRows={handleViewableRows}
+        handleDateRange={handleDateRange}
         sortDescriptor={sortDescriptor}
-        onSortChange={sort}
-        key={tableKey}
-      >
-        <TableHeader>
-          <TableColumn key="date" allowsSorting>Date</TableColumn>
-          <TableColumn key="task" allowsSorting>Task</TableColumn>
-          <TableColumn key="time_tracked" allowsSorting>Time</TableColumn>
-          <TableColumn key="owner" allowsSorting>Owner</TableColumn>
-        </TableHeader>
-        <TableBody>
-          {items.map((row: TimeEntryProps) => (
-            <TableRow key={row.entry_id}>
-              <TableCell>{formatDate(row.date)}</TableCell>
-              <TableCell>{row.task}</TableCell>
-              <TableCell>{convertTime(row.time_tracked)}</TableCell>
-              <TableCell>{row.owner}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+        setSortDescriptor={setSortDescriptor}
+        setTimeEntries={setTimeEntries}
+        timeEntries={timeEntries}
+      />
+
+      <TableDisplay handleSelectedKeys={handleSelectedKeys} items={items} sortDescriptor={sortDescriptor} onSort={sort} />
+
       <div className="flex justify-between">
         {selectedKeys && selectedKeys.length > 0 &&
           <div>
-            <Button variant="flat" color="warning" onPress={() => deleteTimeEntry()}>Remove Selected</Button>
+            <Button
+              variant="flat"
+              color="warning"
+              onPress={() => deleteTimeEntry()}>
+              Remove Selected
+            </Button>
           </div>
         }
+
         <div className="ml-auto">
           <h2 className="text-2xl">
-            <strong>{selectedKeys && selectedKeys.length > 0 ? 'Selected: ' : 'Total: '}</strong>
+            <strong>
+              {selectedKeys && selectedKeys.length > 0 ? 'Selected: ' : 'Total: '}
+            </strong>
             {convertTime(calculatedTime.toString())}
           </h2>
         </div>
+
       </div>
+
       <SubmitTime client={client} />
+
       {viewableRows != -1 &&
-        <Pagination
-          className="flex justify-center"
-          color="primary"
-          variant="light"
-          page={page}
-          total={pages}
-          onChange={(page) => setPage(page)}
-          showControls={true}
-          key={paginationKey}
-          dotsJump={10}
-          boundaries={5}
-        />
+        <PaginateTable page={page} pages={pages} setPage={setPage} paginationKey={paginationKey} />
       }
+
     </div>
   );
 };
