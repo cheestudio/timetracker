8aba6da4ff93fd4441a4cec5811d9387278d4f93
Lars Faye <lars@goodchee.com>
2024-01-19 22:21:13 -0500

    client dropdown

--- src/components/ListClients.tsx
+++ src/components/ListClients.tsx
@@ -5,7 +5,7 @@ const ListClients = ({ clients }: { clients: any }) => {
     <div className="grid grid-cols-3 gap-x-10 gap-y-5">
       {clients.map((client: any) => (
         <div className="mb-5" key={client.client_name}>
-          <Button className="w-full" variant="flat" color="primary" as={Link} href={`/timesheets/?client=${client.id}&week=1`}>{client.client_name}</Button>
+          <Button className="w-full" variant="flat" color="primary" as={Link} href={`/timesheets/?client=${client.id}`}>{client.client_name}</Button>
         </div>
       ))}
     </div>
