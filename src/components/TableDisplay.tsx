f330bf263c8968264fc98a30f6be3cb29df65277
Lars Faye <lars@goodchee.com>
2024-01-31 22:24:11 -0500

    feature: click to copy, condensed view

--- src/components/TableDisplay.tsx
+++ src/components/TableDisplay.tsx
@@ -1,22 +1,36 @@
 import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, SortDescriptor, Button } from "@nextui-org/react";
 import { TimeEntryProps } from "@/lib/types";
 import { formatDate, convertTime, UTCtoLocal } from "@/lib/utils";
-import { CheckCircleIcon } from "@heroicons/react/24/outline";
+import { CheckCircleIcon, ClipboardCopyIcon } from "@heroicons/react/24/outline";
 import EditEntry from "./EditEntry";
+import toast from 'react-hot-toast';
 
 const TableDisplay = ({ items, sortDescriptor, onSort, handleSelectedKeys }: { items: any, sortDescriptor: SortDescriptor, onSort: any, handleSelectedKeys: any }) => {
+
+
+  const CopyToClipboard = ({ text }) => {
+    console.log(text);
+    navigator.clipboard.writeText(text);
+    toast.success('Copied to clipboard!');
+  };
+
   return (
     <Table
       selectionMode="multiple"
       sortDescriptor={sortDescriptor}
       onSelectionChange={handleSelectedKeys}
       onSortChange={onSort}
+      classNames={
+        {
+          td: 'py-0',
+        }
+      }
     >
       <TableHeader>
         <TableColumn key="task" allowsSorting width="25%">Task</TableColumn>
         <TableColumn key="date" allowsSorting>Date</TableColumn>
-        <TableColumn key="time_tracked" allowsSorting>Duration</TableColumn>
         <TableColumn key="start_time" allowsSorting>Time</TableColumn>
+        <TableColumn key="time_tracked" allowsSorting>Duration</TableColumn>
         <TableColumn key="owner" allowsSorting>Owner</TableColumn>
         <TableColumn key="billable" allowsSorting>Billable</TableColumn>
         <TableColumn key="client_name" allowsSorting width="10%">Client</TableColumn>
@@ -25,11 +39,15 @@ const TableDisplay = ({ items, sortDescriptor, onSort, handleSelectedKeys }: { i
       <TableBody>
         {items.map((row: TimeEntryProps) => (
           <TableRow key={row.entry_id}>
-            <TableCell>{row.task}</TableCell>
-            <TableCell>{formatDate(row.date)}</TableCell>
             <TableCell>
-              <div>{convertTime(parseInt(row.time_tracked))}</div>
+              <Button variant="light" disableRipple className="justify-start p-0 rounded-none !bg-transparent !transform-none" onClick={(e) => {
+              navigator.clipboard.writeText(row.task);
+              toast.success('Copied to clipboard!');
+             }}>
+              {row.task}
+             </Button>
             </TableCell>
+            <TableCell>{formatDate(row.date)}</TableCell>
             <TableCell>
               <div className="text-sm">
                 {row.start_time && row.end_time &&
@@ -40,8 +58,11 @@ const TableDisplay = ({ items, sortDescriptor, onSort, handleSelectedKeys }: { i
                 }
               </div>
             </TableCell>
+            <TableCell>
+              <div>{convertTime(parseInt(row.time_tracked))}</div>
+            </TableCell>
             <TableCell>{row.owner}</TableCell>
-            <TableCell>{ row.billable && <CheckCircleIcon className="w-5 h-5 text-primary" /> }</TableCell>
+            <TableCell>{row.billable && <CheckCircleIcon className="w-5 h-5 text-primary" />}</TableCell>
             <TableCell>{row.client_name}</TableCell>
             <TableCell>
               <EditEntry entryData={row} />
